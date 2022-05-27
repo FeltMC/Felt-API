f5b8cd9f43fac42d1588809fba9aa0109c92e487
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-05-27 17:05:17 -0400

    added tool enchantment stuff to tool api

---
+++ felt-tool-api/src/main/java/io/github/feltmc/feltapi/mixin/tool/common/EnchantmentMixin.java
@@ -0,0 +1,19 @@
+package io.github.feltmc.feltapi.mixin.tool.common;
+
+import io.github.feltmc.feltapi.api.tool.extensions.EnchantmentExtension;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Enchantment.class)
+public class EnchantmentMixin {
+    @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
+    public void felt_isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> callback){
+        if (stack.getItem() instanceof EnchantmentExtension extension){
+            callback.setReturnValue(extension.canApplyAtEnchantingTable(stack, (Enchantment) (Object)this));
+        }
+    }
+}
