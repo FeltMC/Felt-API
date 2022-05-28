f71c3723a0b1ec99a6b71a5f7b1229750dd72c23
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-05-28 13:52:41 -0400

    switched to modify variable

--- felt-tool-api/src/main/java/io/github/feltmc/feltapi/mixin/tool/client/ClientPlayerInteractionManagerMixin.java
+++ felt-tool-api/src/main/java/io/github/feltmc/feltapi/mixin/tool/client/ClientPlayerInteractionManagerMixin.java
@@ -11,19 +11,25 @@ import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
-    @Inject(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;shouldCancelInteraction()Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    @ModifyVariable(method = "interactBlock", at = @At(value = "STORE"), ordinal = 1)
+    private boolean modifyBL2(boolean bl2, ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hitResult){
+        return bl2;
+    }
+
+    /*@Inject(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;shouldCancelInteraction()Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
     private void injectInteractBlock(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir, BlockPos blockPos, ItemStack itemStack, boolean bl){
-       /* ItemStack mainStack = player.getMainHandStack();
+       *//* ItemStack mainStack = player.getMainHandStack();
         ItemStack offHandStack = player.getOffHandStack();
         if (mainStack.getItem() instanceof DamageableItemExtension extension){
             bl2 = bl2 && !extension.doesSneakBypassUse(mainStack, world, blockPos, player);
         } else if (offHandStack.getItem() instanceof DamageableItemExtension extension){
             bl2 = bl2 && !extension.doesSneakBypassUse(offHandStack, world, blockPos, player);
-        }*/
-    }
+        }*//*
+    }*/
 }
