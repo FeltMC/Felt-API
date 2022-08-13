336a01498fcf5643de7dbb53e54e01c47646a042
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-08-13 16:38:57 -0400

    added recipe api

---
+++ felt-recipe-api/src/main/java/io/github/feltmc/feltapi/mixin/recipe/AbstractFurnaceBlockEntityMixin.java
@@ -0,0 +1,29 @@
+package io.github.feltmc.feltapi.mixin.recipe;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(AbstractFurnaceBlockEntity.class)
+public class AbstractFurnaceBlockEntityMixin {
+    @Redirect(method = "burn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
+    private static void injectAmount(ItemStack instance, int increment, Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
+        instance.grow(recipe.getResultItem().getCount());
+    }
+
+    @ModifyReturnValue(method = "canBurn", at = @At(value = "RETURN", ordinal = 5))
+    private static boolean canFit(boolean original, @Nullable Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
+        return nonNullList.get(2).getCount() + recipe.getResultItem().getCount() <= nonNullList.get(2).getMaxStackSize();
+    }
+
+    @ModifyReturnValue(method = "canBurn", at = @At(value = "RETURN", ordinal = 4))
+    private static boolean canFit2(boolean original, @Nullable Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
+        return nonNullList.get(2).getCount() + recipe.getResultItem().getCount() <= i;
+    }
+}
---
+++ felt-recipe-api/src/main/java/io/github/feltmc/feltapi/mixin/recipe/SimpleCookingSerializerMixin.java
@@ -0,0 +1,38 @@
+package io.github.feltmc.feltapi.mixin.recipe;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SimpleCookingSerializer.class)
+public class SimpleCookingSerializerMixin {
+    @Shadow @Final private int defaultCookingTime;
+
+    @Shadow @Final private SimpleCookingSerializer.CookieBaker<AbstractCookingRecipe> factory;
+
+    @Inject(method = "fromJson(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonObject;)Lnet/minecraft/world/item/crafting/AbstractCookingRecipe;", at = @At(value = "HEAD"), cancellable = true)
+    private void injectStackSupport(ResourceLocation recipeId, JsonObject json, CallbackInfoReturnable<AbstractCookingRecipe> cir){
+        String string = GsonHelper.getAsString(json, "group", "");
+        JsonElement jsonElement = GsonHelper.isArrayNode(json, "ingredient") ? GsonHelper.getAsJsonArray(json, "ingredient") : GsonHelper.getAsJsonObject(json, "ingredient");
+        Ingredient ingredient = Ingredient.fromJson(jsonElement);
+        JsonElement element = json.get("result");
+        if (element instanceof JsonObject object){
+            ItemStack itemStack = ShapedRecipe.itemStackFromJson(object);
+            float f = GsonHelper.getAsFloat(json, "experience", 0.0F);
+            int i = GsonHelper.getAsInt(json, "cookingtime", this.defaultCookingTime);
+            cir.setReturnValue(this.factory.create(recipeId, string, ingredient, itemStack, f, i));
+        }
+    }
+}
