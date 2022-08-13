65a925544239d0384be8b3e0ad9a49b79af718d5
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-08-13 17:24:17 -0400

    fixed compile errors and mixin.json

--- felt-recipe-api/src/main/java/io/github/feltmc/feltapi/mixin/recipe/AbstractFurnaceBlockEntityMixin.java
+++ felt-recipe-api/src/main/java/io/github/feltmc/feltapi/mixin/recipe/AbstractFurnaceBlockEntityMixin.java
@@ -1,10 +1,10 @@
 package io.github.feltmc.feltapi.mixin.recipe;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.core.NonNullList;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.util.collection.DefaultedList;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,18 +12,18 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
-    @Redirect(method = "burn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
-    private static void injectAmount(ItemStack instance, int increment, Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
-        instance.grow(recipe.getResultItem().getCount());
+    @Redirect(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;increment(I)V"))
+    private static void injectAmount(ItemStack instance, int increment, Recipe<?> recipe, DefaultedList<ItemStack> nonNullList, int i){
+        instance.increment(recipe.getOutput().getCount());
     }
 
-    @ModifyReturnValue(method = "canBurn", at = @At(value = "RETURN", ordinal = 5))
-    private static boolean canFit(boolean original, @Nullable Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
-        return nonNullList.get(2).getCount() + recipe.getResultItem().getCount() <= nonNullList.get(2).getMaxStackSize();
+    @ModifyReturnValue(method = "canAcceptRecipeOutput", at = @At(value = "RETURN", ordinal = 5))
+    private static boolean canFit(boolean original, @Nullable Recipe<?> recipe, DefaultedList<ItemStack> nonNullList, int i){
+        return nonNullList.get(2).getCount() + recipe.getOutput().getCount() <= nonNullList.get(2).getMaxCount();
     }
 
-    @ModifyReturnValue(method = "canBurn", at = @At(value = "RETURN", ordinal = 4))
-    private static boolean canFit2(boolean original, @Nullable Recipe<?> recipe, NonNullList<ItemStack> nonNullList, int i){
-        return nonNullList.get(2).getCount() + recipe.getResultItem().getCount() <= i;
+    @ModifyReturnValue(method = "canAcceptRecipeOutput", at = @At(value = "RETURN", ordinal = 4))
+    private static boolean canFit2(boolean original, @Nullable Recipe<?> recipe, DefaultedList<ItemStack> nonNullList, int i){
+        return nonNullList.get(2).getCount() + recipe.getOutput().getCount() <= i;
     }
 }
