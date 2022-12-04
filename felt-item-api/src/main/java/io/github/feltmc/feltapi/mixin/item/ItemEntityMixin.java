28f1ed98af37d0f45ece145c796bac9a81464da7
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-04 17:29:23 -0500

    finished EntityCustomItem implementation

--- felt-item-api/src/main/java/io/github/feltmc/feltapi/mixin/item/ItemEntityMixin.java
+++ felt-item-api/src/main/java/io/github/feltmc/feltapi/mixin/item/ItemEntityMixin.java
@@ -1,11 +1,18 @@
 package io.github.feltmc.feltapi.mixin.item;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.feltmc.feltapi.api.item.extensions.EntityCustomItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -15,7 +22,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Debug(export = true)
 @Mixin(ItemEntity.class)
-public class ItemEntityMixin {
+public abstract class ItemEntityMixin extends Entity {
+    public ItemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Shadow public abstract ItemStack getStack();
+
     //todo event support and finish lifespan references
     @Unique
     public int lifespan = 6000;
@@ -28,5 +41,20 @@ public class ItemEntityMixin {
     @ModifyConstant(method = "tick", constant = @Constant(intValue = 6000))
     private int redirectConstant(int constant){
         return this.lifespan;
+    }
+
+    @ModifyConstant(method = "setDespawnImmediately", constant = @Constant(intValue = 5999))
+    private int redirectSetDespawnImmediately(int constant){
+        if (getStack().getItem() instanceof EntityCustomItem customItem) return customItem.getEntityLifespan(getStack(), this.world) - 1;
+        return constant;
+    }
+
+    @WrapOperation(method = "damage",at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;onItemEntityDestroyed(Lnet/minecraft/entity/ItemEntity;)V"))
+    private void wrapOnItemEntityDestroyed(ItemStack instance, ItemEntity entity, Operation<Void> original, DamageSource source, float amount){
+        if (instance.getItem() instanceof EntityCustomItem customItem){
+            customItem.onItemEntityDestroyed(entity, source);
+            return;
+        }
+        original.call(entity);
     }
 }
