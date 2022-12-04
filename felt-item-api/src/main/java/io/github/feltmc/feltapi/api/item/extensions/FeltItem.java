9fa61db8b5b8aa02d9b4266014a07af1e9a9bdd2
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-04 15:54:21 -0500

    removed unnessecary code

--- felt-item-api/src/main/java/io/github/feltmc/feltapi/api/item/extensions/FeltItem.java
+++ felt-item-api/src/main/java/io/github/feltmc/feltapi/api/item/extensions/FeltItem.java
@@ -1,18 +1,6 @@
 package io.github.feltmc.feltapi.api.item.extensions;
 
-import io.github.fabricators_of_create.porting_lib.extensions.ItemExtensions;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public interface FeltItem extends FabricItem, ArmorEquipItem, ArmorTickItem, ContainerItem, ElytraFlightItem, EnchantabilityItem, EntityCustomItem, IsDamageableItem, ItemGroupItem, MiscExtension, PiglinCurrencyItem, ShareTagItem, SneakBypassUseItem, PortingLibInterfaces {
-
-    @Nullable
-    @Override
-    default FoodComponent getFoodComponenet(ItemStack stack, @Nullable LivingEntity entity) {
-        return MiscExtension.super.getFoodComponenet(stack, entity);
-    }
 }
