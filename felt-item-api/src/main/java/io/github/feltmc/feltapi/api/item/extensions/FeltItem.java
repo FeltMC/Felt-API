9695183f48712a89a1e91ac5c7b8f21cd7f739b0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-04 23:37:41 -0500

    added missing interfaces to FeltItem

--- felt-item-api/src/main/java/io/github/feltmc/feltapi/api/item/extensions/FeltItem.java
+++ felt-item-api/src/main/java/io/github/feltmc/feltapi/api/item/extensions/FeltItem.java
@@ -2,5 +2,5 @@ package io.github.feltmc.feltapi.api.item.extensions;
 
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 
-public interface FeltItem extends FabricItem, ArmorEquipItem, ArmorTickItem, ContainerItem, ElytraFlightItem, EnchantabilityItem, EntityCustomItem, IsDamageableItem, ItemGroupItem, DroppedByPlayerItem, PiglinCurrencyItem, RepairableItem, ShareTagItem, SneakBypassUseItem, PortingLibInterfaces {
+public interface FeltItem extends FabricItem, ArmorEquipItem, BlockBreakResetItem, BookEnchantableItem, ArmorTickItem, ContainerItem, DroppedByPlayerItem, ElytraFlightItem, EnchantabilityItem, EnderMaskItem, EntityCustomItem, HighlightTipItem, HorseArmorTickItem, IsDamageableItem, ItemGroupItem, PiglinCurrencyItem, RepairableItem, ShareTagItem, SneakBypassUseItem, StackFoodItem, PortingLibInterfaces {
 }
