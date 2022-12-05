c26848e539a3ad1300e652c5fb62e701862b7f15
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-04 20:47:50 -0500

    renamed AnvilMenuMixin

--- felt-item-api/src/main/java/io/github/feltmc/feltapi/mixin/item/AnvilScreenHandlerMixin.java
+++ felt-item-api/src/main/java/io/github/feltmc/feltapi/mixin/item/AnvilScreenHandlerMixin.java
@@ -1,7 +1,6 @@
 package io.github.feltmc.feltapi.mixin.item;
 
 import io.github.feltmc.feltapi.api.item.extensions.BookEnchantableItem;
-import io.github.feltmc.feltapi.api.item.extensions.FeltItem;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -17,7 +16,7 @@ import java.util.Map;
 
 @Debug(export = true)
 @Mixin(AnvilScreenHandler.class)
-public class AnvilMenuMixin {
+public class AnvilScreenHandlerMixin {
     @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;set(I)V", ordinal = 5), locals = LocalCapture.CAPTURE_FAILHARD)
     private void injectBookCheck(CallbackInfo ci, ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map){
         if (itemStack2.getItem() instanceof BookEnchantableItem item){
