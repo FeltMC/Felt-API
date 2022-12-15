581fe33bd84738bfc0f1536616ebd9e3b08d0e1e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-15 17:48:42 -0500

    added armor api

---
+++ felt-armor-api/src/main/java/io/github/feltmc/feltapi/api/armor/ArmorTickItem.java
@@ -0,0 +1,13 @@
+package io.github.feltmc.feltapi.api.armor;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public interface ArmorTickItem {
+    /**
+     * Called to tick armor in the armor slot. Override to do something
+     */
+    default void onArmorTick(ItemStack stack, World level, PlayerEntity player) {
+    }
+}
