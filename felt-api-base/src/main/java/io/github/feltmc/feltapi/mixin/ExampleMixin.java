bb3b4b7e4b671289158964a15cbc78ba1be9d533
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-20 15:29:01 -0500

    fixed dumb gradle boolean quirk

--- felt-api-base/src/main/java/io/github/feltmc/feltapi/mixin/ExampleMixin.java
+++ felt-api-base/src/main/java/io/github/feltmc/feltapi/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package io.github.feltmc.feltapi.mixin;
 
-import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(TitleScreen.class)
