7ab0206e6c79211119c2c2bf492362d095e8e6c8
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-20 14:38:51 -0500

    added method to do subprojects in the modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,19 @@ try {
     for (f in path.listFiles()!!) {
         if (f.isDirectory) {
             if (!strings.contains(f.name)) {
-                include(f.name)
+                val subprojects = File(f, "submodules.txt")
+                if(subprojects.exists()){
+                    val subStrings: List<String> = java.nio.file.Files.lines(subprojects.toPath()).toList()
+                    for (s in subStrings){
+                        val subproject = File(f, s)
+                        if(subproject.exists()){
+                            include(f.name + ":" + s)
+                            project(":" + f.name + ":" + s).projectDir = subproject
+                        }
+                    }
+                } else {
+                    include(f.name)
+                }
             }
         }
     }
