a9b6fa801cc8a13fe94b35f2ef1624137b2df8d0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-17 20:33:03 -0400

    switched from javax nonnull to hetbrains notnull since loom 1.3 no longer seems to provide it

--- felt-mob-item-api/src/main/java/io/github/feltmc/feltapi/api/mobitem/SweepHitBoxItem.java
+++ felt-mob-item-api/src/main/java/io/github/feltmc/feltapi/api/mobitem/SweepHitBoxItem.java
@@ -4,8 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Box;
-
-import javax.annotation.Nonnull;
+import org.jetbrains.annotations.NotNull;
 
 public interface SweepHitBoxItem {
     /**
@@ -16,8 +15,8 @@ public interface SweepHitBoxItem {
      * @param target the entity targeted by the attack.
      * @return the bounding box.
      */
-    @Nonnull
-    default Box getSweepHitBox(@Nonnull ItemStack stack, @Nonnull PlayerEntity player, @Nonnull Entity target)
+    @NotNull
+    default Box getSweepHitBox(@NotNull ItemStack stack, @NotNull PlayerEntity player, @NotNull Entity target)
     {
         return target.getBoundingBox().expand(1.0D, 0.25D, 1.0D);
     }
